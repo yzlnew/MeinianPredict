d9d742db70102a25e94d8548affefb84eabfa39f
yzlnew <yzlnew@gmail.com>
2018-05-04 21:40:15 +0800

    add convert_mixed_num

--- source/selection_utils.py
+++ source/selection_utils.py
@@ -19,5 +19,19 @@ def get_low_importance(filename):
 
     return low_list
 
+def convert_mixed_num(data):
+    try:
+        ret = float(data)
+        return ret if data >=0 else np.nan    # 保证没有负数
+    except:
+        all_match = re.findall(r'\d+\.?\d*', data)  # 注意：不带负号
+
+        if all_match:
+            all_list = [float(i) for i in all_match]
+
+            return sum(all_list) / len(all_list)    # 取均值
+        else:
+            return np.nan
+
 if __name__ == '__main__':
    print(get_low_importance('../model/gbdt_model2018-05-03_1853_4_0.029917.txt'))
