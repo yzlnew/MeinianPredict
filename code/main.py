8305389027a7b9b4860f0d9b5ed4bebbe44d6589
yzlnew <yzlnew@gmail.com>
2018-05-07 17:24:14 +0800

    fix filename typo

--- code/main.py
+++ code/main.py
@@ -5,8 +5,8 @@ import os
 
 def main():
     os.system('python data_cleaning.py')
-    os.system('python data_preprocesing_num.py')
-    os.system('python data_preprocesing_cate.py')
+    os.system('python data_preprocessing_num.py')
+    os.system('python data_preprocessing_cate.py')
     os.system('python model_gbdt.py')
 
 if __name__ == '__main__':
